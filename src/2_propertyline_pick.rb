ef6f6cf07418055598b9a571f67575aedf7cd607
ludwigpeking <ludwigpeking@gmail.com>
2024-08-11 09:12:36 +0800

    property line keyword added

--- src/2_propertyline_pick.rb
+++ src/2_propertyline_pick.rb
@@ -5,6 +5,10 @@ module Real_Estate_Optimizer
       model = Sketchup.active_model
       selection = model.selection
 
+      # Prompt user for keyword
+      keyword = UI.inputbox(["请键入该用地边界线的名称 Enter a keyword for this property line:"], [""], "Property Line Keyword")[0]
+      return if keyword.nil? || keyword.empty?
+
       if selection.empty?
         UI.messagebox("未选择任何图元。 No selection. Please select some edges or a component.")
         return
@@ -14,7 +18,7 @@ module Real_Estate_Optimizer
         component = selection.first
         if component_contains_closed_loop?(component)
           area = calculate_component_area(component)
-          handle_property_line_component(component, area)
+          handle_property_line_component(component, area, keyword)
           UI.messagebox("Area: #{convert_to_square_meters(area).round(2)} square meters")
         else
           UI.messagebox("The selected component does not contain a valid closed loop of edges.")
@@ -23,7 +27,7 @@ module Real_Estate_Optimizer
         edges = selection.grep(Sketchup::Edge)
         if edges_form_closed_loop?(edges)
           area = calculate_area(edges)
-          define_property_line_component(edges, area)
+          define_property_line_component(edges, area, keyword)
           UI.messagebox("Area: #{convert_to_square_meters(area).round(2)} square meters")
         else
           UI.messagebox("请选择一个闭合曲线")
@@ -41,9 +45,9 @@ module Real_Estate_Optimizer
       calculate_area(edges)
     end
 
-    def self.handle_property_line_component(component, area)
+    def self.handle_property_line_component(component, area, keyword)
       model = Sketchup.active_model
-      definition_name = "property_line"
+      definition_name = "property_line_#{keyword}"
       existing_definition = model.definitions[definition_name]
 
       if existing_definition
@@ -55,24 +59,25 @@ module Real_Estate_Optimizer
           end
         end
       else
-        # Rename component definition to 'property_line' if it does not exist
+        # Rename component definition
         component.definition.name = definition_name
       end
 
-      # Set the dynamic attribute 'property_area'
+      # Set the dynamic attributes
       component.definition.set_attribute('dynamic_attributes', '_area_label', 'Property Area')
       component.definition.set_attribute('dynamic_attributes', '_area_units', 'square meters')
       component.definition.set_attribute('dynamic_attributes', 'property_area', convert_to_square_meters(area))
+      component.definition.set_attribute('dynamic_attributes', 'keyword', keyword)
       # Force the dynamic attributes to refresh
       component.definition.set_attribute('dynamic_attributes', '_hasdc', 'true')
       $dc_observers.get_latest_class.redraw_with_undo(component)
 
-      puts "Component '#{component.definition.name}' has been updated with area: #{convert_to_square_meters(area)}"
+      puts "Component '#{component.definition.name}' has been updated with area: #{convert_to_square_meters(area)} and keyword: #{keyword}"
     end
 
-    def self.define_property_line_component(edges, area)
+    def self.define_property_line_component(edges, area, keyword)
       model = Sketchup.active_model
-      definition_name = "property_line"
+      definition_name = "property_line_#{keyword}"
       definition = model.definitions[definition_name] || model.definitions.add(definition_name)
 
       # Clear any existing entities in the definition
@@ -90,15 +95,16 @@ module Real_Estate_Optimizer
       instance = model.active_entities.add_instance(definition, Geom::Transformation.new)
       instance.name = definition_name
 
-      # Set the dynamic attribute 'property_area'
+      # Set the dynamic attributes
       instance.definition.set_attribute('dynamic_attributes', '_area_label', 'Property Area')
       instance.definition.set_attribute('dynamic_attributes', '_area_units', 'square meters')
       instance.definition.set_attribute('dynamic_attributes', 'property_area', convert_to_square_meters(area))
+      instance.definition.set_attribute('dynamic_attributes', 'keyword', keyword)
       # Force the dynamic attributes to refresh
       instance.definition.set_attribute('dynamic_attributes', '_hasdc', 'true')
       $dc_observers.get_latest_class.redraw_with_undo(instance)
 
-      puts "Instance '#{instance.name}' has been created with area: #{convert_to_square_meters(area)}"
+      puts "Instance '#{instance.name}' has been created with area: #{convert_to_square_meters(area)} and keyword: #{keyword}"
     end
 
     def self.edges_form_closed_loop?(edges)
--- src/toolbar.rb
+++ src/toolbar.rb
@@ -1,11 +1,12 @@
 require_relative '1_input'
+require_relative '2_propertyline_pick'
+require_relative '3_basement_pick'
 require_relative '4_apartment_manager'
 require_relative '5_building_generator'
-require_relative '3_basement_pick'
-require_relative '2_propertyline_pick'
 require_relative '6_insert_building'
-require_relative '8_output'
 require_relative '7_building_attribute_editor'
+require_relative '8_output'
+
 
 
 module Real_Estate_Optimizer
@@ -25,8 +26,8 @@ module Real_Estate_Optimizer
       }
       cmd_input.small_icon = "../icons/input.png"
       cmd_input.large_icon = "../icons/input.png"
-      cmd_input.tooltip = "项目基本信息输入 Project Inputs"
-      cmd_input.status_bar_text = "项目基本信息输入 Project Inputs"
+      cmd_input.tooltip = "项目基本信息输入"
+      cmd_input.status_bar_text = "Project Inputs"
       toolbar.add_item(cmd_input)
 
       cmd_propertyline_pick = UI::Command.new("Propertyline Pick") {
@@ -34,7 +35,7 @@ module Real_Estate_Optimizer
       }
       cmd_propertyline_pick.small_icon = "../icons/propertyline_pick.png"
       cmd_propertyline_pick.large_icon = "../icons/propertyline_pick.png"
-      cmd_propertyline_pick.tooltip = "拾取用地红线 Propertyline Pick"
+      cmd_propertyline_pick.tooltip = "拾取用地红线"
       cmd_propertyline_pick.status_bar_text = "Propertyline Pick"
       toolbar.add_item(cmd_propertyline_pick)
 
@@ -43,7 +44,7 @@ module Real_Estate_Optimizer
       }
       cmd_basement_pick.small_icon = "../icons/basement_pick.png"
       cmd_basement_pick.large_icon = "../icons/basement_pick.png"
-      cmd_basement_pick.tooltip = "拾取地下轮廓线 Basement Pick"
+      cmd_basement_pick.tooltip = "拾取地下轮廓线"
       cmd_basement_pick.status_bar_text = "Basement Pick"
       toolbar.add_item(cmd_basement_pick)
 
@@ -52,7 +53,7 @@ module Real_Estate_Optimizer
       }
       cmd_apartment_manager.small_icon = "../icons/apartment.png"
       cmd_apartment_manager.large_icon = "../icons/apartment.png"
-      cmd_apartment_manager.tooltip = "户型管理 Manage Apartment Types"
+      cmd_apartment_manager.tooltip = "户型管理"
       cmd_apartment_manager.status_bar_text = "Add or manage apartment types."
       toolbar.add_item(cmd_apartment_manager)
 
@@ -61,7 +62,7 @@ module Real_Estate_Optimizer
       }
       cmd_building_type.small_icon = "../icons/building_type.png"
       cmd_building_type.large_icon = "../icons/building_type.png"
-      cmd_building_type.tooltip = "楼型管理 Generate Building"
+      cmd_building_type.tooltip = "楼型管理"
       cmd_building_type.status_bar_text = "Generates a new building."
       toolbar.add_item(cmd_building_type)
 
@@ -73,7 +74,7 @@ module Real_Estate_Optimizer
       }
       cmd_insert_building.small_icon = "../icons/insert_building.png"
       cmd_insert_building.large_icon = "../icons/insert_building.png"
-      cmd_insert_building.tooltip = "选择楼型插入模型 Insert Building"
+      cmd_insert_building.tooltip = "选择楼型插入模型"
       cmd_insert_building.status_bar_text = "Insert Building"
       toolbar.add_item(cmd_insert_building)
 
@@ -82,7 +83,7 @@ module Real_Estate_Optimizer
       }
       cmd_edit_building_attributes.small_icon = "../icons/building_attribute_editor.png"
       cmd_edit_building_attributes.large_icon = "../icons/building_attribute_editor.png"
-      cmd_edit_building_attributes.tooltip = "标段时间计划和属性 Edit Building Attributes"
+      cmd_edit_building_attributes.tooltip = "标段时间计划和属性"
       cmd_edit_building_attributes.status_bar_text = "Edit attributes of selected building components"
       toolbar.add_item(cmd_edit_building_attributes)
 
@@ -91,8 +92,8 @@ module Real_Estate_Optimizer
       }
       cmd_output.small_icon = "../icons/output.png"
       cmd_output.large_icon = "../icons/output.png"
-      cmd_output.tooltip = "输出财务指标和信息 Output"
-      cmd_output.status_bar_text = "Output"
+      cmd_output.tooltip = "输出财务指标和信息"
+      cmd_output.status_bar_text = "KPI/cashflow Output"
       toolbar.add_item(cmd_output)
 
       # Ensure the toolbar is visible
